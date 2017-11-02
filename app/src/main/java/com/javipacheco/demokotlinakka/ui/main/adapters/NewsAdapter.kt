9ec9932b2d075f670b9e4d2197e778c6fab5ef02
Javi Pacheco <javi.pacheco@gmail.com>
2017-11-02 16:28:10 +0100

    CardView with images

--- app/src/main/java/com/javipacheco/demokotlinakka/ui/main/adapters/NewsAdapter.kt
+++ app/src/main/java/com/javipacheco/demokotlinakka/ui/main/adapters/NewsAdapter.kt
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.javipacheco.demokotlinakka.R
 import com.javipacheco.demokotlinakka.models.Events
 import kategory.ListKW
@@ -16,6 +17,11 @@ class NewsAdapter(val items: ListKW<Events.RedditNewsDataEvent>): RecyclerView.A
         fun populate(item: Events.RedditNewsDataEvent) {
             itemView.news_title.setText(item.title)
             itemView.news_author.setText(item.author)
+            Glide
+                    .with(itemView.context)
+                    .load(item.thumbnail)
+                    .centerCrop()
+                    .into(itemView.news_photo);
         }
 
     }
